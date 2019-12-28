5affd5251136bd252cfba086a775972a95b6d982
Baurzhan Dosym <baurzhan.zh@gmail.com>
2019-12-28 14:46:09 -0500

    lesson7

--- first.py
+++ first.py
@@ -1,1 +1,53 @@
-print("hello, world")
+the_list=[1,5,3,3,"word"]
+# TUPLES
+# the_tuple = (1,5,3,3,"word")
+# print(len(the_tuple))
+#
+# print(the_tuple[1])
+#
+# numbers = 1,3,5
+# print(numbers)
+# print(type(numbers))
+#
+# for x in the_tuple:
+#     print(x)
+#
+# a,b,c=numbers
+# print(c)
+# print(numbers[-1])
+#
+# print(the_tuple+numbers)
+
+# SETS
+# the_set = {1,4,"word","snake","word"}
+# for x in the_set:
+#     print(x)
+# print(the_set)
+# the_set.add("banana")
+# print(the_set)
+#
+# the_set.update(the_list)
+# print(the_set)
+# print(len(the_set))
+# print(type(the_set))
+# the_set.remove("snake")
+# print(the_set)
+# the_set.discard("banana")
+# print(the_set)
+# # the_set.remove("snake")
+# the_set.discard("banana")
+# print(the_set)
+
+# list []
+# tuple ()
+# set {}
+# dictionary {}
+
+x=100
+# while x>10:
+#     x=a=x-1
+#     print(a)
+
+print(f"I want to print {x}")
+some_words = "new world"
+print(f"new company name = {some_words} and it will cost {x} dollars")
