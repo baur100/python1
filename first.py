48672cb065df7f9ca2ed004accc849c5dce36e20
Taras Podolskiy <tpodolskiyqa@gmail.com>
2019-12-28 14:52:50 -0500

    lesson 7

--- first.py
+++ first.py
@@ -1,1 +1,7 @@
-print("Hello World Tar")
+# LISTS     variable[value]
+# TUPLES    variable(value) cannot edit, add etc
+# SETS      variable{value} throws random values not in a listed order, prints only unique numbers, if 2 values are the same it will show only one
+# DISCTIONARY    ???
+
+
+
