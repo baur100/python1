89131af7d8b732ac5fe5f8781661bb8c7f5898dc
Taras Podolskiy <tpodolskiyqa@gmail.com>
2020-01-09 22:30:14 -0500

    homework11 //taras

--- homeworks/homework11.py
+++ homeworks/homework11.py
@@ -1,12 +1,10 @@
 print('#1...\n из numbers создайте новый лист все элементы которого будут numbers в квадрате')
-print()
 numbers = [2, 1, 3, 5, 6, 7, 4, 5, 8, 18, 9]
 squared = list(map(lambda s: s ** 2, numbers))
 print(squared)
 print()
 
 print('#2...\n из colors создайте новый лист все элементы которого будут написаны заглавными буквами')
-print()
 colors = ['red', 'green', 'blue', 'pink', 'black', 'brown', 'grey', 'white', 'yellow']
 for items in colors:
     print(items.upper())
@@ -14,7 +12,6 @@ print('^ list with .upper() method')
 print()
 
 print('#3... \n из numbers создайте новый лист который содержит только четные и меньше 7')
-print()
 numbers = [2, 1, 3, 5, 6, 7, 4, 5, 8, 18, 9]
 for num in numbers:
     if num < 7 and num % 2 == 0:
