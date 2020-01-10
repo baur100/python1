8efc85b3936d42f3074929f741c89a6133dbd5de
Taras Podolskiy <tpodolskiyqa@gmail.com>
2020-01-10 10:53:38 -0500

    homework11 //taras redone

--- homeworks/homework11.py
+++ homeworks/homework11.py
@@ -1,27 +1,24 @@
-print('#1...\n из numbers создайте новый лист все элементы которого будут numbers в квадрате')
 numbers = [2, 1, 3, 5, 6, 7, 4, 5, 8, 18, 9]
+colors = ['red', 'green', 'blue', 'pink', 'black', 'brown', 'grey', 'white', 'yellow']
+print()
+
+print('#1...\n из numbers создайте новый лист все элементы которого будут numbers в квадрате')
 squared = list(map(lambda s: s ** 2, numbers))
 print(squared)
 print()
 
 print('#2...\n из colors создайте новый лист все элементы которого будут написаны заглавными буквами')
-colors = ['red', 'green', 'blue', 'pink', 'black', 'brown', 'grey', 'white', 'yellow']
-for items in colors:
-    print(items.upper())
-print('^ list with .upper() method')
-print()
+print(f"unmodified list - {colors}")
+upCol = list(map(lambda c: c.upper(), colors))
+print(upCol)
 
 print('#3... \n из numbers создайте новый лист который содержит только четные и меньше 7')
-numbers = [2, 1, 3, 5, 6, 7, 4, 5, 8, 18, 9]
-for num in numbers:
-    if num < 7 and num % 2 == 0:
-        print(num)
-print(' ^ this prints all nums that are < 7 and are even')
-print()
+evnNums = list(filter(lambda n: (not n%2) and n < 7, numbers))
+print(evnNums)
+
 
 print('#4... \n из colors создайте новый лист который будет в котором будет цвета начинающиеся на \'b\' или имеющие 4 буквы ')
 print('The following statements filter values by letter \'b\' ')
-colors = ['red', 'green', 'blue', 'pink', 'black', 'brown', 'grey', 'white', 'yellow']
 b_mapped = list(filter(lambda x: x[0] == 'b', colors))
 print(b_mapped)
 
