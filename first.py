ec07c215326b57ef576baef462bf7f3729eb4e59
Baurzhan Dosym <baurzhan.zh@gmail.com>
2020-01-10 22:03:22 -0500

    Prime numbers

--- first.py
+++ first.py
@@ -1,35 +1,15 @@
+def is_prime(x):
+    for i in range(2,x-1):
+        if x%i==0:
+            return False
+    return True
 
 
-def method(p):
-    for x in p:
-        if(x>2):
-            print(x)
+def find_prime(x,y):
+    for z in range(x,y):
+        if is_prime(z):
+            print(z)
 
-x = lambda y : y*2
 
-print(x(11))
-
-print(x(6))
-
-number = lambda x,y,z : z*2+y*3+z*4
-
-print(number(1,2,3))
-
-spisok = [3,4,5,3,1,6]
-
-double_it = list(map(lambda x: x-3, spisok))
-print(double_it)
-method(double_it)
-
-months = ['jan', 'feb','may', 'mar', 'june','jul']
-j_months = []
-# j_months = list(filter(lambda z: z[0]=='j',months))
-for x in months:
-    if x[0]=='j':
-        j_months.append(x)
-m_months = list(filter(lambda v:len(v)==3,months))
-
-print(m_months)
-print(j_months)
-
+find_prime(250,300)
 
