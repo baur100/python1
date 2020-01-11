6cd08bad2822c55ada3d8b658d8de055182287f0
Baurzhan Dosym <baurzhan.zh@gmail.com>
2020-01-10 22:40:43 -0500

    Lesson 12 Errors

--- first.py
+++ first.py
@@ -1,15 +1,34 @@
-def is_prime(x):
-    for i in range(2,x-1):
-        if x%i==0:
-            return False
-    return True
-
-
-def find_prime(x,y):
-    for z in range(x,y):
-        if is_prime(z):
-            print(z)
-
-
-find_prime(250,300)
-
+#
+# numbers = [2,1,3,5,6,7,4,5,8,18,9]
+# dict = {"ff":"xx"}
+#
+# # print (x) NameError
+# # print(numbers[100]) IndexError:
+# # x = numbers + 'hello' TypeError
+# # int("foo") ValueError
+# # dict["cc"] KeyError
+# # "hello".foo AttributeError
+# # 5/0 ZeroDivisionError
+#
+# def get_birth_year(age):
+#     if type(age) is not int:
+#         raise TypeError("Argument must be integer")
+#     if age<0 or age >125:
+#         raise ValueError("Wrong age entered")
+#     print(2020-age)
+#
+#
+# try:
+#     get_birth_year("gi")
+#
+# # except TypeError:
+# #     print("wrong Type entered")
+# except:
+#     print("got error")
+# # else:
+# #     print("we in else")
+# # finally:
+# #     print("finallyregergergergre")
+#
+#
+# # print("finish")
