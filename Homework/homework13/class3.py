4f412e68fb9a3e7966651c3c2c0333a9f89194e4
Vladimir21331 <vladimir2133@gmail.com>
2020-01-14 14:08:56 -0800

    HW13

--- Homework/homework13/class3.py
+++ Homework/homework13/class3.py
@@ -1,4 +1,4 @@
-class Dealership ():
+class Dealership():
     brand = ""
     production_year = 0
     date = 2020
@@ -6,15 +6,12 @@ class Dealership ():
     options = []
     price = 0
 
-
-
-
     def print_info(self):
-        print ( f"    brand - {self.brand} ")
-        print ( f"    production  year - {self.production_year} ")
-        print ( f"    color - {self.color} ")
-        print ( f"    price - ${self.price} ")
-        print (f"    your car is  {self.get_car_age() }" + "   years old ")
+        print(f"    brand - {self.brand} ")
+        print(f"    production  year - {self.production_year} ")
+        print(f"    color - {self.color} ")
+        print(f"    price - ${self.price} ")
+        print(f"    your car is  {self.get_car_age()}" + "   years old ")
 
         self.get_options()
 
@@ -22,8 +19,9 @@ class Dealership ():
         print("    options  -  ", *self.options)
 
     def get_car_age(self):
+
+        if self.date - self.production_year < 0 or self.date - self.production_year >100:
+            raise ValueError("НЕВЕРНЫЕ ИСХОДНЫЕ ДАННЫЕ")
+
+        else:
             return self.date - self.production_year
-
-
-
-
--- Homework/homework13/main.py
+++ Homework/homework13/main.py
@@ -2,11 +2,9 @@ from Homework.homework13.class1 import Bank
 from Homework.homework13.class2 import Kindergarden
 from Homework.homework13.class3 import Dealership
 
-
 print("")
 print("class1")
 print("")
-
 
 customer_1 = Bank()
 customer_1.first_name_on_account = " John"
@@ -22,8 +20,6 @@ customer_2.balance = 7000
 customer_2.change_balance(-500)
 customer_2.all_accounts = ["checking ,", " savings"]
 
-
-
 customer_1.print_info()
 print("")
 
@@ -32,7 +28,6 @@ customer_2.print_info()
 print("")
 print("class2")
 print("")
-
 
 child_1 = Kindergarden()
 child_1.first_name = "Dima"
@@ -44,8 +39,7 @@ child_2 = Kindergarden()
 child_2.first_name = "Nikolai"
 child_2.second_name = "Terentiev"
 child_2.DOB = 2016
-child_2.favorite_food = ["outmeal ,","yogurt"]
-
+child_2.favorite_food = ["outmeal ,", "yogurt"]
 
 child_1.print_info()
 print("")
@@ -59,14 +53,14 @@ print("")
 car_1 = Dealership()
 car_1.brand = "BMW"
 car_1.color = "white"
-car_1.options = ["sunroof ," , "adaptive CC ," , "sport package"]
+car_1.options = ["sunroof ,", "adaptive CC ,", "sport package"]
 car_1.price = 62500.00
 car_1.production_year = 2018
 
 car_2 = Dealership()
 car_2.brand = "Ford"
 car_2.color = "red"
-car_2.options = ["moonroof ," , "Intelligent 4 Wheel Drive," , "Voice-Activated Touchscreen Navigation System"]
+car_2.options = ["moonroof ,", "Intelligent 4 Wheel Drive,", "Voice-Activated Touchscreen Navigation System"]
 car_2.price = 23999.00
 car_2.production_year = 2019
 
@@ -75,17 +69,6 @@ car_1.print_info()
 print("")
 
 car_2.print_info()
-
-# try:
-#     car_1.print_info()
-#
-# except ValueError:
-#     print("WRONG DATA!")
-#
-# try:
-#     car_2.print_info()
-# except ValueError:
-#     print("Wrong data entered!")
 
 
 
