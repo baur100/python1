6c795ce9350bd4937123f0d049874b0a113b4f08
gulmiram <gulmiram@testpro.io>
2020-01-15 12:33:09 -0500

    Overdue HW 7

---
+++ HomeWork7/HomeWork.py
@@ -0,0 +1,47 @@
+#a=(3,5,1,'snow',6)
+#print(a)
+#a= tuple("snow")
+#print(a)
+#a=(3,5,1,'snow',6)
+#print(a)
+#for x in a:
+ #   print(x)
+#a= (3,5,1,'snow',6)
+#print(a [3:])
+#print(a[1])
+
+
+
+#set={2,3,"cat",2,"cat"}
+#print (set)
+#set.add("apple")
+#print(set)
+#for x in set:
+   # print(x)
+#set.add("mango")
+#print(set)
+#set.pop()
+#print(set)
+#set.remove("cat")
+#print(set)
+#set.discard(2)
+#print(set)
+
+ #x=40
+#while x>10:
+   # x=a=x-1
+    #print(a)
+#i=1000
+#while i>100:
+   # print(i)
+    #i/=2
+
+#x= "what is f"
+#print(f"I need to understand {x}?!")
+#confusing="this f"
+#print(f"why so hard to get {confusing}! just wanna be able to use  {x}.")
+
+
+
+
+
