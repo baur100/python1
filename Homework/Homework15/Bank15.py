c69a28beb6ef5e956e3e0993797a857ff68494cb
Vladimir21331 <vladimir2133@gmail.com>
2020-01-17 11:49:00 -0800

    HW15

--- Homework/Homework15/Bank15.py
+++ Homework/Homework15/Bank15.py
@@ -18,6 +18,22 @@ class Bank:
             raise ValueError("too much")
         self._balance = i
 
+    @property
+    def all_accounts(self):
+        return self._all_accounts
+
+    @all_accounts.setter
+    def all_accounts(self,value):
+        self._all_accounts=value
+
+    @property
+    def first_name_on_account(self):
+        return self._first_name_on_account
+
+    @first_name_on_account.setter
+    def first_name_on_account(self,j):
+        self._first_name_on_account = j
+
     def __repr__(self):
         return (
             f"{self._first_name_on_account}   {self._second_name_on_account}   has  {self._all_accounts} accounts in the bank, and his balance is  {self.balance}")
